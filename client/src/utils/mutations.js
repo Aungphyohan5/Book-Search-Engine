e570931ecda08acde2e027c1cad19b7ed8f72268
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-04-04 08:41:14 -0300

    added usemutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,81 @@
+import { gql } from '@apollo/client';
+
+
+export const CREATE_USER = gql`
+mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        password
+        username
+        savedBooks {
+          _id
+          title
+          authors
+          description
+          image
+          link
+        }
+      }
+    }
+  }
+  `
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+        savedBooks {
+          _id
+          title
+          authors
+          description
+          image
+          link
+        }
+      }
+    }
+  }`
+
+export const SAVE_BOOK = gql`
+ mutation SaveBook($bookId: ID!, $description: String!, $authors: [String], $title: String, $image: String, $link: String) {
+    saveBook(bookId: $bookId, description: $description, authors: $authors, title: $title, image: $image, link: $link) {
+      _id
+      email
+      password
+      username
+      savedBooks {
+        _id
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }`
+
+export const DELETE_BOOK = gql`
+  mutation DeleteBook($userId: ID!, $bookId: ID!) {
+    deleteBook(userId: $userId, bookId: $bookId) {
+      _id
+      email
+      password
+      username
+      savedBooks {
+        _id
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }`
