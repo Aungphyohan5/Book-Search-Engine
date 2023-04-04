118660f14f9f3f2dee5ed970010ac62872a897a5
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-04-04 08:41:45 -0300

    added usequery

---
+++ client/src/utils/queries.js
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+query User($userId: ID!) {
+    user(id: $userId) {
+      email
+      password
+      savedBooks {
+        _id
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`
+
+export const QUERY_SINGLEUSER = gql`
+query User($userId: ID!) {
+    user(id: $userId) {
+      _id
+      email
+      password
+      username
+      savedBooks {
+        _id
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }`
+
